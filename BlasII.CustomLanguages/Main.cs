28d633e559a564191ecf6ff4d25b89c763ee09e4
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-06-04 12:01:18 -0400

    init: add base project files

---
+++ BlasII.CustomLanguages/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.CustomLanguages;
+
+internal class Main : MelonMod
+{
+    public static CustomLanguages CustomLanguages { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        CustomLanguages = new CustomLanguages();
+    }
+}
---
+++ BlasII.CustomLanguages/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using BlasII.CustomLanguages;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(BlasII.CustomLanguages.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
