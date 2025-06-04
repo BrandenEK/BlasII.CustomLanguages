cbb87c9d899f4f62e2c1818b63fd18700a544686
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-06-04 12:05:07 -0400

    core: add project info

--- BlasII.CustomLanguages/CustomLanguages.cs
+++ BlasII.CustomLanguages/CustomLanguages.cs
@@ -2,6 +2,9 @@
 
 namespace BlasII.CustomLanguages;
 
+/// <summary>
+/// Allows using custom translations made by the community
+/// </summary>
 public class CustomLanguages : BlasIIMod
 {
     internal CustomLanguages() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
