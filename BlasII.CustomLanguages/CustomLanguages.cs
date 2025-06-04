1d226f2f0cc39897f5cb78410f26d4fffb7bb0bc
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-06-04 12:21:20 -0400

    lang: search through custom folder

--- BlasII.CustomLanguages/CustomLanguages.cs
+++ BlasII.CustomLanguages/CustomLanguages.cs
@@ -1,4 +1,5 @@
 ﻿using BlasII.ModdingAPI;
+using System.IO;
 
 namespace BlasII.CustomLanguages;
 
@@ -9,8 +10,25 @@ public class CustomLanguages : BlasIIMod
 {
     internal CustomLanguages() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-    protected override void OnInitialize()
+    /// <summary>
+    /// Loads all languages from the custom_languages folder
+    /// </summary>
+    protected override void OnAllInitialized()
     {
-        // Perform initialization here
+        string langDir = Path.Combine(FileHandler.ModdingFolder, "custom_languages");
+        Directory.CreateDirectory(langDir);
+
+        ModLog.Info($"Loading custom languages from {langDir}");
+        foreach (string dir in Directory.GetDirectories(langDir))
+        {
+            LoadLanguage(dir);
+        }
+    }
+
+    private void LoadLanguage(string dir)
+    {
+        string langCode = Path.GetFileName(dir);
+        ModLog.Info($"Loading language: {langCode}");
+
     }
 }
